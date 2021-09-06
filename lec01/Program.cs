bdbb6d66012a3e51f2c16c75cc1dc837a075484e
Deniz Isik <31687755+Emreisik46@users.noreply.github.com>
2021-09-06 13:23:25 +0200

    added system input

--- lec01/Program.cs
+++ lec01/Program.cs
@@ -6,22 +6,46 @@ namespace lec01
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+
+            try
+            {
+                string input = System.Console.ReadLine();
+                int inputToInt = Int32.Parse(input);  
+                 
+                System.Console.WriteLine(program.isLeapYear(inputToInt));
+            }
+            catch (System.FormatException e)
+            {
+                System.Console.WriteLine("You may only type numbers as input!");
+            
+            }
+            catch (System.Exception e)
+            {
+                 System.Console.WriteLine("We have caught an exception. Please contact the developers!");
+           
+            }
+            
+
+           
             
         }
     
 
         public bool isLeapYear(int year)
         {
+            
             if (year < 1582)
             {
-                System.Console.WriteLi
-             return false;   
+                System.Console.WriteLine("The year must be greater than 1582");
+                return false;   
             }
-        if ((year/4) % 4 == 0)
-        {
-              return true;
+
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+            return false; 
          }
-        return false; 
-     }
     }
 }
